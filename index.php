97084274c32882870e052fc90e6a224aa577ff1d
dvincem <dvincem@che>
2023-02-13 15:39:09 +0000

    final

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
         <title>
-            title sample
+            HAPPY VALENTINES DAY BABI 😘💜
         </title>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="style.css">
